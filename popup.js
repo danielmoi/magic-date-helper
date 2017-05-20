5243070884868fdd7a3d87813d35b044e30a0fa2
danielmoi <daniel@ampersandmoi.com>
2017-05-20 12:21:48 +1000

    Fix types for timestamp conversion

--- popup.js
+++ popup.js
@@ -1,30 +1,72 @@
+/* --- Now Panel ------------------------ */
 const renderNowDates = () => {
-  document.getElementById('now-local').textContent = new Date().toString();
-  document.getElementById('now-iso').textContent = new Date().toISOString();
-  document.getElementById('now-utc').textContent = new Date().toUTCString();
-  document.getElementById('now-milli').textContent = new Date().getTime();
+  document.getElementById('mdh-now-local').textContent = new Date().toString();
+  document.getElementById('mdh-now-iso').textContent = new Date().toISOString();
+  document.getElementById('mdh-now-utc').textContent = new Date().toUTCString();
+  document.getElementById('mdh-now-milli').textContent = new Date().getTime();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   renderNowDates();
 });
 
-document.getElementById('to-utc').addEventListener('click', () => {
-  const value = document.getElementById('input').value;
-  document.getElementById('converted').textContent = new Date(value).toUTCString();
+
+/* --- Convert Panel ------------------------ */
+// helpers
+const removeActiveClasses = () => {
+  const buttons = document.getElementsByClassName('mdh-button');
+  for (const button of buttons) {
+    button.classList.remove('mdh-button--active');
+  }
+};
+
+const addActiveClass = (el) => {
+  el.classList.add('mdh-button--active');
+};
+
+
+// add click listeners
+document.getElementById('mdh-to-utc').addEventListener('click', () => {
+  let value = document.getElementById('mdh-input').value;
+  const numbersOnly = /^[0-9]*$/;
+  if (value.match(numbersOnly)) {
+    value = Number(value);
+  }
+  document.getElementById('mdh-converted').textContent = new Date(value).toUTCString();
 });
 
-document.getElementById('to-local').addEventListener('click', () => {
-  const value = document.getElementById('input').value;
-  document.getElementById('converted').textContent = new Date(value);
+document.getElementById('mdh-to-local').addEventListener('click', () => {
+  let value = document.getElementById('mdh-input').value;
+  const numbersOnly = /^[0-9]*$/;
+  if (value.match(numbersOnly)) {
+    value = Number(value);
+  }
+  document.getElementById('mdh-converted').textContent = new Date(value);
 });
 
-document.getElementById('to-iso').addEventListener('click', () => {
-  const value = document.getElementById('input').value;
-  document.getElementById('converted').textContent = new Date(value).toISOString();
+document.getElementById('mdh-to-iso').addEventListener('click', () => {
+  let value = document.getElementById('mdh-input').value;
+  const numbersOnly = /^[0-9]*$/;
+  if (value.match(numbersOnly)) {
+    value = Number(value);
+  }
+  document.getElementById('mdh-converted').textContent = new Date(value).toISOString();
 });
 
-document.getElementById('to-milli').addEventListener('click', () => {
-  const value = document.getElementById('input').value;
-  document.getElementById('converted').textContent = new Date(value).getTime();
+document.getElementById('mdh-to-milli').addEventListener('click', () => {
+  let value = document.getElementById('mdh-input').value;
+  const numbersOnly = /^[0-9]*$/;
+  if (value.match(numbersOnly)) {
+    value = Number(value);
+  }
+  document.getElementById('mdh-converted').textContent = new Date(value).getTime();
 });
+
+const buttons = document.getElementsByClassName('mdh-button');
+for (const button of buttons) {
+  button.addEventListener('click', function () {
+    removeActiveClasses();
+    addActiveClass(this);
+  });
+}
+
