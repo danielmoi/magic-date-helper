3c201077e88c421e02aeb6a47c2780f68bc834b3
danielmoi <daniel@ampersandmoi.com>
2017-05-20 14:29:58 +1000

    Improve accessibility - increase color contrast - update manifest - add bash script for zipping

--- popup.js
+++ popup.js
@@ -24,49 +24,30 @@ const addActiveClass = (el) => {
   el.classList.add('mdh-button--active');
 };
 
+const getDisplay = (id, value) => {
+  switch (id) {
+    case 'mdh-to-utc': return new Date(value).toUTCString();
+    case 'mdh-to-local': return new Date(value);
+    case 'mdh-to-iso': return new Date(value).toISOString();
+    case 'mdh-to-milli': return new Date(value).getTime();
+    default: return '';
+  }
+};
 
 // add click listeners
-document.getElementById('mdh-to-utc').addEventListener('click', () => {
-  let value = document.getElementById('mdh-input').value;
-  const numbersOnly = /^[0-9]*$/;
-  if (value.match(numbersOnly)) {
-    value = Number(value);
-  }
-  document.getElementById('mdh-converted').textContent = new Date(value).toUTCString();
-});
-
-document.getElementById('mdh-to-local').addEventListener('click', () => {
-  let value = document.getElementById('mdh-input').value;
-  const numbersOnly = /^[0-9]*$/;
-  if (value.match(numbersOnly)) {
-    value = Number(value);
-  }
-  document.getElementById('mdh-converted').textContent = new Date(value);
-});
-
-document.getElementById('mdh-to-iso').addEventListener('click', () => {
-  let value = document.getElementById('mdh-input').value;
-  const numbersOnly = /^[0-9]*$/;
-  if (value.match(numbersOnly)) {
-    value = Number(value);
-  }
-  document.getElementById('mdh-converted').textContent = new Date(value).toISOString();
-});
-
-document.getElementById('mdh-to-milli').addEventListener('click', () => {
-  let value = document.getElementById('mdh-input').value;
-  const numbersOnly = /^[0-9]*$/;
-  if (value.match(numbersOnly)) {
-    value = Number(value);
-  }
-  document.getElementById('mdh-converted').textContent = new Date(value).getTime();
-});
-
 const buttons = document.getElementsByClassName('mdh-button');
+
 for (const button of buttons) {
-  button.addEventListener('click', function () {
+  button.addEventListener('click', function magic() {
     removeActiveClasses();
     addActiveClass(this);
+    let value = document.getElementById('mdh-input').value;
+    const numbersOnly = /^[0-9]*$/;
+    if (value.match(numbersOnly)) {
+      value = Number(value);
+    }
+    const id = this.id;
+    const display = getDisplay(id, value);
+    document.getElementById('mdh-converted').textContent = display;
   });
 }
-
